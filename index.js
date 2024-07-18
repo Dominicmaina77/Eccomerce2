5e8814599a57398678f390486f3ac7bd71d9998a
Dominicmaina77 <mainadominic628@gamil.com>
2024-07-18 22:20:56 +0300

    Ecommerce2

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const productRoutes = require ('./route/ProductRoute');
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config({
+        path: './.env'
+    });
+}
+
+const port = process.env.PORT;
+const dbconnect = "mongodb+srv://Dominic:Dominic77.@cluster0.jvxtzpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.listen(port,(req,res) => {
+    console.log(`Server is running on ${port}`);
+    console.log(`database url is ${dbconnect}`);
+})
+
+
+mongoose.connect(dbconnect,{useNewUrlParser:true,useUnifiedTopology:true})
+   .then(() => console.log("Connected to MongoDB"))
+   .catch(err => console.error('Error connecting to MongoDB',err));
+
+   app.use('/api/products',productRoutes);
